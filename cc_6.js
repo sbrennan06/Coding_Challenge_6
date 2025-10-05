9fb54709c7982434d2b0b8ab0a221dff9b202db2
Samantha Brennan <samanthabrennan@usf.edu>
2025-10-05 17:18:41 -0400

    add describe for Manager subclass

--- cc_6.js
+++ cc_6.js
@@ -7,13 +7,15 @@ class Employee {
         this.department = department;
     }
     describe() {
-        return 
-    `${this.name} works in ${this.department}.`; 
+        return `${this.name} works in ${this.department}.`; 
     }
 }
 
 class Manager extends Employee {
     constructor(name, department, teamSize) {
-        super(name, department); //super calls constructor from parent class\
+        super(name, department); //super calls constructor from parent class
+    }
+    describe() {
+        return `${this.name} manages a team of ${this.teamSize} people in ${this.department}`;
     }
 }
