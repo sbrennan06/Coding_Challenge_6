2a1b1a0b266e9b566dba5d4e4cd0c10500e2b2b6
Samantha Brennan <samanthabrennan@usf.edu>
2025-10-05 17:10:39 -0400

    created base class

--- cc_6.js
+++ cc_6.js
@@ -1,1 +1,13 @@
 //Coding Challenge 6
+
+//create base class
+class Employee {
+    constructor(name, department) {
+        this.name =  name;
+        this.department = department;
+    }
+    describe() {
+        return 
+    `${this.name} works in ${this.department}.`; 
+    }
+}
